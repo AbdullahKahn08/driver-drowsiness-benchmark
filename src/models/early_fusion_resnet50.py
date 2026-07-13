7ba5864122cd0eecf0df927438dc5982bddff506
Abdullah <eddiebroker957@gmail.com>
2026-07-13 18:52:59 +0500

    Frozen the early layers of dual stream ResNet

--- src/models/early_fusion_resnet50.py
+++ src/models/early_fusion_resnet50.py
@@ -25,6 +25,12 @@ class EarlyFusionResNet(torch.nn.Module):
             mouth_backbone.layer2
         )
 
+        for param in self.eye_branch.parameters():
+            param.requires_grad = False
+
+        for param in self.mouth_branch.parameters():
+            param.requires_grad = False
+
         self.fusion_conv = torch.nn.Conv2d(1024,512,kernel_size=1)
 
         self.shared_layer = torch.nn.Sequential(
@@ -34,3 +40,13 @@ class EarlyFusionResNet(torch.nn.Module):
         )
 
         self.fc = torch.nn.Linear(2048,2)
+    
+    def forward(self,eyes_input,mouths_input):
+        eye_features = self.eye_branch(eyes_input)
+        mouth_features = self.mouth_branch(mouths_input)
+        features_concatente = torch.cat([eye_features,mouth_features],dim=1)
+        early_fusion = self.fusion_conv(features_concatente)
+        combined_features = self.shared_layer(early_fusion)
+        combined_features = torch.flatten(combined_features,1)
+        output = self.fc(combined_features)
+        return output
