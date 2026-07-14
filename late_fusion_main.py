2080343d00103ee797a616ea5281fca0ff3bd278
Kahn <eddiebroker957@gmail.com>
2026-07-14 18:41:44 +0100

    Changed the files and created new ones for late fusion

---
+++ late_fusion_main.py
@@ -0,0 +1,30 @@
+import torch
+from src.models.late_fusion_resnet50 import LateFusionResNet
+from src.data.dual_stream_dataloader import get_dataloaders
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from src.training.dual_stream_train import train
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    #GLOBAL PARAMETERS
+    PATH = r"data/processed"
+    LEARNING_RATE = 0.0001
+    BATCH_SIZE = 32
+    NUM_OF_EPOCHS = 20
+
+
+    training,val,test = get_dataloaders(path=PATH,batchSize=BATCH_SIZE)
+
+    model = LateFusionResNet()
+
+    loss_function = CrossEntropyLoss()
+
+    optimzer =  Adam(filter(lambda p: p.requires_grad,model.parameters()), lr=LEARNING_RATE)
+
+    scheduler = ReduceLROnPlateau(optimizer=optimzer,mode='min',patience=5,factor=0.5)
+
+    train(model=model,train_dataloader=training,val_dataloader=val,loss_fn=loss_function,optimizer=optimzer,num_epochs=NUM_OF_EPOCHS,device=device,scheduler=scheduler)
+
