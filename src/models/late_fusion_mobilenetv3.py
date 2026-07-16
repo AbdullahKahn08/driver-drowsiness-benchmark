5c416b0c174bde702f0c478149a32ebf2552f146
Abdullah <eddiebroker957@gmail.com>
2026-07-17 01:29:15 +0500

    Wrote late fusion

--- src/models/late_fusion_mobilenetv3.py
+++ src/models/late_fusion_mobilenetv3.py
@@ -4,7 +4,27 @@ from torchvision.models import mobilenet_v3_large
 class LateFusionMobileNetv3(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        pass
+        eye_backbone = mobilenet_v3_large(weights="IMAGENET1K_V2")
+        mouth_backbone = mobilenet_v3_large(weights="IMAGENET1K_V2")
+
+        self.eye_branch = eye_backbone.features[0:17]
+        self.mouth_branch = mouth_backbone.features[0:17]
+
+        for param in self.eye_branch[0].parameters():
+            param.requires_grad = False
+
+        for param in self.eye_branch[0].parameters():
+            param.requires_grad = False
+
+        self.avgpool = eye_backbone.avgpool
+
+        self.fc = torch.nn.Linear(1920,2)
 
     def forward(self,eyes_input,mouth_input):
-        pass
+        eye_features = self.eye_branch(eyes_input)
+        mouth_features = self.mouth_branch(mouth_input)
+        combined_features = torch.cat([eye_features,mouth_features],dim=1)
+        combined_features = self.avgpool(combined_features)
+        combined_features = torch.flatten(combined_features,1)
+        output = self.fc(combined_features)
+        return output
