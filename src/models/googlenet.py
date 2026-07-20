b0b22b2eb20a3c18d100ad32d6bff695cdd573f4
Abdullah <eddiebroker957@gmail.com>
2026-07-20 06:35:34 +0500

    Wrote the simple googlenet file

---
+++ src/models/googlenet.py
@@ -0,0 +1,13 @@
+import torch.nn
+from torchvision.models import googlenet
+
+def get_googlenet():
+    model = googlenet(weight="IMAGENET1K_V1")
+    
+    for param in model.conv1.parameters():
+        param.requires_grad = False
+
+    model.fc = torch.nn.Linear(1024,2)
+
+    return model
+
